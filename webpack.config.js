53e278aed4b7f829f2490ef404914d04fb0ce01b
Catherine Aguilera <catherineaguilera13@gmail.com>
2021-07-09 16:51:13 -0400

    'fixing babel bug'

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,13 @@ module.exports = {
             {
             test: /\.js$/i,
             exclude: /node_modules/,
-            use: {loader: "babel-loader"}
-            }
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env']
+                    },
+                },
+            },
         ]
-    }
+    },
 };
